09bbf8486c7600142a2112754bf7e89610d853f9
Geovannni Almeida <131813766+gezinnn@users.noreply.github.com>
2024-08-06 15:35:31 -0300

    TesteGIT

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -4,6 +4,7 @@ import styles from './LandingPage.module.css';
 export default function LandingPage() {
   return (
     <div className={styles.container}>
+      aaaaa
       <button className={styles.botao} onClick={() => (window.location.href = "/home")}>Ir Para aa Home</button>
     </div>
   );
